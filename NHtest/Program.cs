e886349aff31dabbbe01ad614b0e5d21e6fc000e
tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2023-08-04 11:35:46 +0200

    tmp

--- NHtest/Program.cs
+++ NHtest/Program.cs
@@ -1,30 +1,24 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+using System.Windows;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+using System.Text;
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
-using FluentNHibernate.Data;
 using FluentNHibernate.Mapping;
+using Microsoft.SqlServer.Management.SqlParser.Parser;
 using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Criterion;
-using NHibernate.Engine;
-using NHibernate.Hql.Ast;
-using NHibernate.Hql.Ast.ANTLR;
-using NHibernate.Impl;
-using NHibernate.Linq;
-using NHibernate.Linq.Functions;
-using NHibernate.Linq.Visitors;
-using NHibernate.Loader.Criteria;
-using NHibernate.Multi;
-using NHibernate.Persister.Entity;
 using NHibernate.SqlCommand;
 using NHibernate.Tool.hbm2ddl;
-using NHibernate.Util;
+using System.Linq.Expressions;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using UnaryExpression = System.Linq.Expressions.UnaryExpression;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace NHtest
 {
@@ -43,31 +37,80 @@ namespace NHtest
                 var customer1 = new Customer1 { CustomerCode = "123", Name = "1", Prop1 = "1", Age = 1 };
                 session.SaveOrUpdate(customer1);
 
-                var customer2 = new Customer2 { CustomerCode = "123", Name = "2", Prop2 = "2", Age = 2 };
+                var customer2 = new CustomerTest { CustomerCode = "123", Name = "2", Prop2 = "2", Age = 2, Addresses = new List<Address>{ new(), new()}};
                 session.SaveOrUpdate(customer2);
                 transaction.Commit();
             }
+            QueryOver(session);
+            //Linq(session);
+            Criteria(session);
+        }
 
-            var query = QueryOver.Of<Customer>().Where(c => c.Name == "1")
-                .And(c => ((Customer1)c).Age == 1)
-                .And(c => ((Customer2)c).Age == 2)
-                .Select(c => c.Name);
+        private static void Linq(ISession session)
+        {
+            var l = session.Query<Customer>()
+                .Where(c => c.Name == "1")
+                .Where(c => ((Customer1)c).Age == 1 || ((CustomerTest)c).Age == 1)
+                .Select(c => c.Id)
+                .ToList();
+            Console.WriteLine(l.Count);
+        }
 
-            foreach (var customer in query.GetExecutableQueryOver(session).List())
+        private static void Criteria(ISession session)
+        {
+            var crit = session.CreateCriteria<Customer>("c")
+                .SetProjection(Projections.Property("Id"))
+                .CreateEntityAlias("cust2", Restrictions.EqProperty("c.Id", "cust2.Id"), JoinType.LeftOuterJoin, typeof(CustomerTest).FullName)
+                //.CreateEntityAlias("cust2", Restrictions.EqProperty("c.Id", "cust2.Id"), JoinType.LeftOuterJoin, typeof(Customer2).FullName)
+                .Add(Restrictions.EqProperty("Age", "cust2.Age"));
+
+            Console.WriteLine(crit.List().Count);
+        }
+
+        private static void QueryOver(ISession session)
+        {
+            Customer cust = null;
+
+            var query = NHibernate.Criterion.QueryOver.Of(() => cust);
+            query.Where(Equal<Customer, Customer1, int>(query, c => c.Age, 1, cust))
+                .Where(Equal<Customer, CustomerTest, int>(query, c => c.Age, 2, cust));
+
+            //query.UnderlyingCriteria.CreateAlias("tblCustomerTest", "test").Add(Restrictions.Eq("test.Age", 123));
+            foreach (var customer in query.Select(c => c.Id).GetExecutableQueryOver(session).List<int>())
             {
                 Console.WriteLine($"{customer}");
             }
         }
 
-        private static QueryOver<Customer, Customer> GetDetachedQuery(Customer customer)
+
+
+
+        private static ICriterion Equal<TRoot, TSubType, TKey>(QueryOver<TRoot, TRoot> query, Expression<Func<TSubType, object>> property, TKey value, Customer cust)
+            where TRoot : Customer
+            where TSubType : TRoot
         {
-            var detachedQuery = QueryOver.Of<Customer>().Where(c => c.Name == customer.Name).Select(c => c.Name);
-            return detachedQuery;
+            Type subType = typeof(TSubType);
+            string name = subType.Name.ToLower();
+
+            query.UnderlyingCriteria
+                .CreateEntityAlias(
+                    name, 
+                    Restrictions.EqProperty(Projections.Property(() => cust.Id), $"{name}.Id"), 
+                    JoinType.LeftOuterJoin,
+                    subType.FullName);
+
+            var propExpression = ((MemberExpression)((UnaryExpression)property.Body).Operand);
+
+            return Restrictions.Eq($"{name}.{propExpression.Member.Name}", value);
         }
 
-        static string FormatSql(string sql)
+
+
+
+        private static QueryOver<Customer, Customer> GetDetachedQuery(Customer customer)
         {
-            return sql;
+            var detachedQuery = NHibernate.Criterion.QueryOver.Of<Customer>().Where(c => c.Name == customer.Name).Select(c => c.Name);
+            return detachedQuery;
         }
 
         private static ISessionFactory CreateSessionFactory()
@@ -97,6 +140,17 @@ namespace NHtest
                 .Create(true, true);
         }
     }
+
+    internal class TokenInfo
+    {
+        public int Start { get; internal set; }
+        public int End { get; internal set; }
+        public bool IsPairMatch { get; internal set; }
+        public bool IsExecAutoParamHelp { get; internal set; }
+        public string Sql { get; internal set; }
+        public Tokens Token { get; internal set; }
+    }
+
     public class Customer
     {
         public virtual int Id { get; set; }
@@ -110,12 +164,36 @@ namespace NHtest
         public virtual int Age { get; set; }
         public virtual string Prop1 { get; set; }
     }
-    public class Customer2 : Customer
+    public abstract class Customer2 : Customer
     {
-        public virtual int Age { get; set; }
-
         public virtual string Prop2 { get; set; }
 
+    }
+    public class Customer21 : Customer2
+    {
+        public virtual int Age { get; set; }
+        public virtual int CustomerId { get; set; }
+
+    }
+    public class CustomerTest : Customer21
+    {
+        public virtual IList<Address> Addresses { get; set; }
+    }
+
+    public class Address
+    {
+        public virtual int AddressId { get; set; }
+    }
+
+    public class AddressMap : ClassMap<Address>
+    {
+        public AddressMap()
+        {
+            Id(x => x.AddressId)
+                .GeneratedBy.Identity();
+            
+            Table("tblAddress");
+        }
     }
     public class CustomerMap : ClassMap<Customer>
     {
@@ -145,21 +223,71 @@ namespace NHtest
         public Customer2Map()
         {
             KeyColumn("Id");
-
-            Map(x => x.Age);
+            
             Map(x => x.Prop2);
             Table("tblCustomer2");
         }
     }
+    public class Customer21Map : SubclassMap<Customer21>
+    {
+        public Customer21Map()
+        {
+            KeyColumn("CustomerId");
+
+            Map(x => x.Age);
+            Table("tblCustomer21");
+        }
+    }
+    public class CustomerTestMap : SubclassMap<CustomerTest>
+    {
+        public CustomerTestMap()
+        {
+            KeyColumn("CustomerId");
+            HasMany(t => t.Addresses)
+                .KeyColumn("Id")
+                .Cascade.AllDeleteOrphan();
+            Table("tblCustomerTest");
+        }
+    }
     public class LoggingInterceptor : EmptyInterceptor
     {
+        TSql120Parser _parser = new(false);
+        Sql120ScriptGenerator _generator = new (new SqlScriptGeneratorOptions()
+        {
+            KeywordCasing = KeywordCasing.Uppercase,
+            IncludeSemicolons = true,
+            NewLineBeforeFromClause = true,
+            NewLineBeforeOrderByClause = true,
+            NewLineBeforeWhereClause = true,
+            AlignClauseBodies = true
+        });
         public override SqlString OnPrepareStatement(SqlString sql)
         {
+            Console.WriteLine("NH: ");
+            var parsedQuery = _parser.Parse(new StringReader(ToString(sql)), out var errors);
 
-            Console.Write("NHibernate: ");
-            Console.WriteLine(sql);
+            _generator.GenerateScript(parsedQuery, out var formattedQuery);
+            Console.WriteLine(formattedQuery);
 
-            return sql;
+            return base.OnPrepareStatement(sql);
+        }
+
+        public string ToString(SqlString sql)
+        {
+            StringBuilder builder = new StringBuilder();
+            int inc = 0;
+            foreach (var part in sql)
+            {
+                if (part is Parameter)
+                {
+                    builder.Append($"@p{inc++}");
+                }
+                else
+                {
+                    builder.Append(part);
+                }
+            }
+            return builder.ToString();
         }
     }
 }
